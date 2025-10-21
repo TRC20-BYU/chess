e786a40cdfa00c9572e961a9e60f82d3501015df
Tyler <tcox1@byu.edu>
2025-10-21 11:37:52 -0500

    started on exceptions

---
+++ server/src/main/java/server/ResponseException.java
@@ -0,0 +1,16 @@
+package server;
+
+public class ResponseException extends Exception {
+
+    public enum Code {
+        ServerError,
+        ClientError,
+    }
+
+    final private Code code;
+
+    public ResponseException(Code code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,12 +1,15 @@
 package server;
 
 import com.google.gson.Gson;
+import dataModel.RegistrationResult;
 import dataModel.UserData;
 import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
 import io.javalin.*;
 import io.javalin.http.Context;
+import passoff.exception.ResponseParseException;
 import service.UserService;
+import server.ResponseException;
 
 import java.util.Map;
 //import json;
@@ -26,22 +29,64 @@ public class Server {
         // Register your endpoints and exception handlers here.
         server.delete("db", this::deleteDatabase);
         server.post("user", this::register);
+        server.post("session", this::login);
+        server.delete("session", this::logout);
 
+    }
 
+    private void deleteDatabase(Context ctx) {
+        userService.deleteDatabase();
     }
 
     private void register(Context ctx) {
         var serializer = new Gson();
         String reqJson = ctx.body();
         var req = serializer.fromJson(reqJson, UserData.class);
-
-        var regResult = userService.register(req);
-        ctx.result(serializer.toJson(regResult));
+        if (req.username() == null && req.password() == null && req.email() == null) {
+            ctx.status(HttpStatus.BAD_REQUEST);
+            ctx.result("Error: bad request");
+        } else {
+            var regResult = userService.register(req);
+            if (regResult == null) {
+                ctx.status(HttpStatus.UNAUTHORIZED);
+                ctx.result("Error: unauthorized");
+            } else {
+                ctx.result(serializer.toJson(regResult));
+            }
+        }
     }
 
-    private void deleteDatabase(Context ctx) {
-        userService.deleteDatabase();
+    private void login(Context ctx) throws ResponseException {
+        System.out.println("here1");
+        var serializer = new Gson();
+        System.out.println("here2");
+        String reqJson = ctx.body();
+        System.out.println("here3");
+        var req = serializer.fromJson(reqJson, UserData.class);
+        System.out.println("here4");
+        if (req.username() == null && req.password() == null) {
+            throw new ResponseException(ResponseException.Code.ClientError, "Error: no dogs with fleas");
+        } else {
+            var regResult = userService.login(req);
+            if (regResult == null) {
+                ctx.result("Error: unauthorized");
+            } else {
+                ctx.result(serializer.toJson(regResult));
+            }
+        }
     }
+
+    private void logout(Context ctx) {
+        var serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(reqJson, RegistrationResult.class);
+        boolean regResult = userService.logout(req.authToken());
+        if (!regResult) {
+            ctx.status(HttpStatus.UNAUTHORIZED);
+            ctx.result("Error: unauthorized");
+        }
+    }
+
 
     public int run(int desiredPort) {
         server.start(desiredPort);
