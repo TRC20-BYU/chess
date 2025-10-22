acf34f3a6246457cd2fa6fbe1d8262303681b594
Tyler <tcox1@byu.edu>
2025-10-21 19:15:19 -0500

    tostring for game data

--- server/src/main/java/dataModel/GameData.java
+++ server/src/main/java/dataModel/GameData.java
@@ -1,4 +1,11 @@
 package dataModel;
 
+import org.jetbrains.annotations.NotNull;
+
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName) {
+    @NotNull
+    @Override
+    public String toString() {
+        return "{\"gameID\": " + gameID + ", \"whiteUsername\":" + whiteUsername + ", \"blackUsername\":" + blackUsername + ", \"gameName:\"" + gameName + "}";
+    }
 }
--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -6,6 +6,7 @@ import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import server.Server;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,4 +34,19 @@ class GameServiceTest {
         Assertions.assertFalse(x > 0);
     }
 
+    @Test
+    void joinGame() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        GameService gameService = new GameService(dataAccess);
+        UserService userService = new UserService(dataAccess);
+        UserData userData = new UserData("Joe", "password", "joe@joe");
+        userService.register(userData);
+        RegistrationResult res = userService.login(userData);
+        int x = gameService.createGame(res.authToken(), "name");
+        int y = gameService.joinGame(res.authToken(), Server.PlayerColor.WHITE, x);
+        Assertions.assertTrue(y > 0);
+        y = gameService.joinGame(res.authToken(), Server.PlayerColor.WHITE, x);
+        Assertions.assertFalse(y > 0);
+
+    }
 }
