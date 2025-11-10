35129cecef70be4e24d8e6f1e3352c76324003db
Tyler <tcox1@byu.edu>
2025-11-10 14:10:17 -0600

    phase 5 started

---
+++ client/src/main/java/ui/PreloginUI.java
@@ -0,0 +1,20 @@
+package ui;
+
+public class PreloginUI {
+
+    void help() {
+
+    }
+
+    void quit() {
+
+    }
+
+    void login() {
+        //http request
+    }
+
+    void register() {
+        //http request
+    }
+}
