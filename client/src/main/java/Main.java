17efdac4125b68a9245e7c23477e21cd596eb7d5
Tyler <tcox1@byu.edu>
2025-11-10 22:51:03 -0600

    add parameter collection and message to indicate logged in and logged out

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -9,30 +9,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome ♕ 240 Chess Client ♕");
         boolean loggedIn = false;
+        String user = "";
         PreloginUI preloginUI = new PreloginUI();
         PostloginUI postloginUI = new PostloginUI();
 
         while (true) {
+            if (!loggedIn) {
+                System.out.print("not logged in: ");
+            } else {
+                System.out.print("logged in as " + user + ": ");
+            }
             Scanner scanner = new Scanner(System.in);
             String line = scanner.nextLine();
+            String[] params = line.split(" ");
             if (!loggedIn) {
-                if (Objects.equals(line, "help")) {
+                if (Objects.equals(params[0], "help")) {
                     preloginUI.help();
                 }
-                if (Objects.equals(line, "login")) {
+                if (Objects.equals(params[0], "login")) {
                     preloginUI.login();
+                    user = params[1];
                     loggedIn = true;
                 }
             } else {
-                if (Objects.equals(line, "help")) {
+                if (Objects.equals(params[0], "help")) {
                     postloginUI.help();
                 }
-                if (Objects.equals(line, "logout")) {
+                if (Objects.equals(params[0], "logout")) {
                     postloginUI.logout();
                     loggedIn = false;
                 }
             }
-            if (Objects.equals(line, "quit")) {
+            if (Objects.equals(params[0], "quit")) {
                 break;
             }
 
