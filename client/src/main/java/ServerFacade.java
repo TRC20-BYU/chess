5eac01ec582bdd180a5075a809dec2c17fc7eff9
Tyler <tcox1@byu.edu>
2025-11-11 23:56:23 -0600

    more progress on the server facade

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,20 +1,23 @@
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ServerFacade {
 
-    public void facade() {
+    private final HttpClient client = HttpClient.newHttpClient();
 
+    public void get(String path, Object body) {
+        var request = buildRequest("get", path, body);
+        sendRequest(request);
     }
 
-    private HttpRequest buildRequest(String method, String path, Object body) {
-        var request = HttpRequest.newBuilder().uri(URI.create("temp")).method(method, makeRequestBody(body));
-        if (body != null) {
 
-        }
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder().uri(URI.create("temp" + path)).method(method, makeRequestBody(body));
         return request.build();
     }
 
@@ -23,6 +26,14 @@ public class ServerFacade {
             return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
         } else {
             return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            return null;
         }
     }
 }
